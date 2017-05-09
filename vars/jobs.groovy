ccd5c9943dac322b9cf5ed60f4af00feddc2a571
Tasneem Rahim <trahim@zaizi.com>
2017-05-09 12:09:06 +0100

    read giturl

--- vars/jobs.groovy
+++ vars/jobs.groovy
@@ -1,14 +1,15 @@
 def getJobs(String environment) {
     def pipelineFile = readProperties file: "pipelines.txt"
     pipelines = pipelineFile['pipelines'].tokenize(',')
+    gitUrl = pipelineFile['gitUrl']
     for (int i=0; i<pipelines.size(); i++) {
         def name = pipelines[i]
-        createJob(name, environment)
+        createJob(name, environment, gitUrl)
         build job: "${environment}/${name}"
     }
 }
 
-def createJob(String name, environment){
+def createJob(String name, environment, gitUrl){
     step([
             $class: "ExecuteDslScripts",
             lookupStrategy: "SEED_JOB",
